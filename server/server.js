5f794b717daab926892692e01ff8a1c386d5a7e9
Richard Johansson <ricjo462@student.liu.se>
2022-04-20 14:06:49 +0200

    Changed to port 5001

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -15,5 +15,5 @@ app.listen(port, () => {
         if (err) console.error(err);
 
     });
-    console.log('Server is running on port: ${port}')
+    console.log('Server is running on port: ', port)
 });
