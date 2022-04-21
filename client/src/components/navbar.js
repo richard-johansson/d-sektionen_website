5a57ccf342ff31bb3aa64c7e62bed474a443d6e6
Richard Johansson <ricjo462@student.liu.se>
2022-04-21 11:17:33 +0200

    WIP top bar not expanding.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,28 +12,27 @@ export default function Navbar() {
    <div>
      <nav className="navbar navbar-expand-lg navbar-light bg-light">
        <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+        <img
+            width = "56"
+            src = {require("./D-logo-color-whiteBG.png")} >
+        </img>
+        Datateknologsektionen
        </NavLink>
        <button
          className="navbar-toggler"
          type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
+         data-bs-toggle="collapse"
+         data-bs-target="#navbarNavAltMarkup"
+         aria-controls="navbarNavAltMarkup"
          aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
+         aria-label="Toggle navigation" >
          <span className="navbar-toggler-icon"></span>
        </button>
- 
-       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
+       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link" href="#">Sökande</a>
+          <a className="nav-item nav-link active" href="#">Företag</a>
+       </div>
        </div>
      </nav>
    </div>
