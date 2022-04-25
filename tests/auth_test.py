64871a154a9c6bc1bdd7bb0f22ea090b71f3a50d
Sean <seazh870@student.liu.se>
2022-04-25 20:34:20 +0200

    made logOut test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
+driver = webdriver.Firefox()
+
 def writeToFile(assertion, output, testName):
     file = open('testOutput.txt', 'a')
     file.write("Test: " + testName + "\n")
@@ -19,3 +21,19 @@ def writeToFile(assertion, output, testName):
     
     file.close()
     return
+
+
+def logout_test():
+    logout_css_selector = "#root > div > div:nth-child(1) > nav > div.navbar-collapse.collapse.w-100.order-3.dual-collapse2 > ul > div > div:nth-child(2) > button"
+
+    driver.find_element(by=By.CSS_SELECTOR, value=logout_css_selector).click()
+
+    output = driver.find_element(by=By.CSS_SELECTOR, value="#root > div > div:nth-child(1) > nav > div.navbar-collapse.collapse.w-100.order-3.dual-collapse2 > ul > div > div:nth-child(2) > button").text
+    assertion = "Logga in"
+
+    writeToFile(assertion, output, "logOutTest")
+    return
+
+
+if __name__ == "__main__":
+    driver.get("http://localhost:3000")
