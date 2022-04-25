c451245df16122780c748283955a5a4e67cd53bf
Sean <seazh870@student.liu.se>
2022-04-25 10:26:33 +0200

    fixed routes

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
  
 // We import NavLink to utilize the react router.
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
  
 // Here, we display our Navbar
 export default function Navbar() {
@@ -37,7 +37,7 @@ export default function Navbar() {
             <a className="nav-link" href="#">Om sektionen</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Medlem</a>
+            <Link className="nav-link" to="/medlem">Medlem</Link>
           </li>
        </ul>
        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Member from "./components/member";
 const container = document.getElementById('root');
 
 const root = createRoot(container);
