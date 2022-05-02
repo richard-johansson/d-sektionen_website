e642850a7cd32c79eac8e9efca7589cc309163b7
Richard Johansson <ricjo462@student.liu.se>
2022-05-02 11:33:13 +0200

    Added scheduler from React Scheduler example

--- client/src/components/logging.js
+++ client/src/components/logging.js
@@ -2,12 +2,15 @@ import React from "react";
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
+import Schedule from "./scheduler";
 
 export default function Logging() {
     return (
         <div className="container">
             <h2>Loggbok</h2>
-                <a>Här kan du logga körda kilometer med någon av sekionens bilar.</a>
+            <a>Här kan du logga körda kilometer med någon av sekionens bilar.</a>
+
+            <Schedule />
         </div>
     );
 }
