235b0d33ced6277c225b0dd9299a7a6eccda6f90
Richard Johansson <ricjo462@student.liu.se>
2022-05-03 11:55:17 +0200

    Added form with input validation of start and stop to be numbers only

--- client/src/components/logging.js
+++ client/src/components/logging.js
@@ -8,6 +8,45 @@ export default function Logging() {
         <div className="container">
             <h2>Loggbok</h2>
             <a>Här kan du logga körda kilometer med någon av sekionens bilar.</a>
+            <ul>
+                <li>
+                    <a>Den som hyr bilen ansvarar för att kontrollera mätarställningen vid start och slut.</a>
+                </li>
+                <li>
+                    <a>Om den som hyr bilen inte loggar körningen kan Datateknologsektionen kräva betalning
+                    för hela den sträcka som saknas i loggboken, även om det är flera personer/utskott som
+                    har hyrt bilen under den perioden.</a>
+                </li>
+            </ul> 
+            <form className="mt-3 needs-validation" novalidate>
+                <div className="mb-3">
+                    <label htmlFor="start-distance" className="form-label">
+                        Mätarställning vid start
+                    </label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="start-distance" 
+                        min="0"
+                        placeholder="t.ex. 12345" 
+                        required>
+                    </input>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="stop-distance" className="form-label">
+                        Mätarställning vid slut
+                    </label>
+                    <input 
+                        type="number" 
+                        className="form-control" 
+                        id="stop-distance"
+                        min="0"
+                        placeholder="t.ex. 12356" 
+                        required>
+                    </input>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 }
