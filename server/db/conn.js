ec856707e7f818751386fb99382fca81f2b7a28c
Richard Johansson <ricjo462@student.liu.se>
2022-05-10 11:49:59 +0200

    Created new database and collection for bookings

--- server/db/conn.js
+++ server/db/conn.js
@@ -13,11 +13,11 @@ module.exports = {
       // Verify we got a good "db" object
       if (db)
       {
-        _db = db.db("employees");
+        _db = db.db("database");
         console.log("Successfully connected to MongoDB."); 
       }
       return callback(err);
-         });
+      });
   },
  
   getDb: function () {
