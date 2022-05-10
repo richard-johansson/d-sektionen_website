8b618d11ece501cab087e233040d522d9845af94
Richard Johansson <ricjo462@student.liu.se>
2022-05-10 11:51:04 +0200

    Started working on backend for handeling bookings

---
+++ server/routes/bookings.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+// bookingsRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting 
+// with path /medlem/boka.
+const bookingsRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+/* Database is structured as follows:
+
+    Booking
+        id          ObjectId
+        startDate   Date
+        endDate     Date
+        title       string
+        notes       string
+        allDay      boolean
+        Person
+            id      ObjectId
+            Name    string
+        Logging
+            id              ObjectId
+            Start mileage   int
+            Stop mileage    int
+            Private         boolean
+            Committee       string
+            Price           double
+            Paid            boolean
+*/
+
+// This section will help you create a new record.
+bookingsRoutes.route("/medlem/boka/ny_bokning").post(function (req, response) 
+{
+    console.log("/medlem/boka/ny_bokning");
+    console.log("id: ", req.params.id);
+    let db_connect = dbo.getDb();
+    let booking = {
+        data: req.body.data
+    };
+    db_connect.collection("bookings").insertOne(booking, function (err, res) 
+    {
+        if (err) throw err;
+        response.json(res);
+    });
+});
+
+module.exports = bookingsRoutes;
