5950a78bd2027e450fe13104bfe08d6a899ca0d4
Richard Johansson <ricjo462@student.liu.se>
2022-05-23 11:18:46 +0200

    Using new booking component

--- client/src/components/bookCar.js
+++ client/src/components/bookCar.js
@@ -2,14 +2,14 @@ import React from "react";
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
-import Schedule from "./scheduler";
+import Bookings from "./scheduler";
 
 export default function BookCar() {
     return (
         <div className="container">
             <h2>Bilbokning</h2>
             <a>Här kan du boka sektionsbilarna.</a>
-            <Schedule />
+            <Bookings />
         </div>
     );
 }
