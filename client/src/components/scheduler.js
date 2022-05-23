b8652b743b1809c75a3a601d2fb20c6e42972912
Richard Johansson <ricjo462@student.liu.se>
2022-05-23 11:17:57 +0200

    Refactored to class components, now we can read from database and edit appointments in frontend

--- client/src/components/scheduler.js
+++ client/src/components/scheduler.js
@@ -1,170 +1,95 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import React, {useState} from 'react';
 import Paper from '@mui/material/Paper';
-import LinearProgress from '@mui/material/LinearProgress';
-import {
-  ViewState,
-} from '@devexpress/dx-react-scheduler';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
-  WeekView,
   DayView,
+  WeekView,
   Appointments,
-  Toolbar,
-  DateNavigator,
-  ViewSwitcher,
   AppointmentForm,
   AppointmentTooltip,
-  TodayButton,
+  ConfirmationDialog,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
-const PREFIX = 'Demo';
 
-const classes = {
-  toolbarRoot: `${PREFIX}-toolbarRoot`,
-  progress: `${PREFIX}-progress`,
-};
+export default class Bookings extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: []
+      };
+      this.commitChanges = this.commitChanges.bind(this);
+    }
 
-const StyledDiv = styled('div')({
-  [`&.${classes.toolbarRoot}`]: {
-    position: 'relative',
-  },
-});
+    componentDidMount() {
+      this.getData();
+    }
 
-const StyledLinearProgress = styled(LinearProgress)(() => ({
-  [`&.${classes.progress}`]: {
-    position: 'absolute',
-    width: '100%',
-    bottom: 0,
-    left: 0,
-  },
-}));
-
-const PUBLIC_KEY = 'AIzaSyBnNAISIUKe6xdhq1_rjor2rxoI3UlMY7k';
-const CALENDAR_ID = 'f7jnetm22dsjc3npc2lu3buvu4@group.calendar.google.com';
-
-const getData = (setData, setLoading) => {
-  // const dataUrl = ['https://www.googleapis.com/calendar/v3/calendars/', CALENDAR_ID, '/events?key=', PUBLIC_KEY].join('');
-  const dataUrl = "http://localhost:5001/medlem/boka/hamta_alla";
-  setLoading(true);
-
-  const schedule = fetch(dataUrl, {headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-      }
-    })
-    .then(response => response.json())
-    .then((data) => {
-      setTimeout(() => {
-        console.log("hejkhej");
-        console.log("dataitems: ", data);
-        setData(data);
-        setLoading(false);
-      }, 600);
-    });
-
-    console.log(schedule);
-    return schedule;
-};
-
-const ToolbarWithLoading = (
-  ({ children, ...restProps }) => (
-    <StyledDiv className={classes.toolbarRoot}>
-      <Toolbar.Root {...restProps}>
-        {children}
-      </Toolbar.Root>
-      <StyledLinearProgress className={classes.progress} />
-    </StyledDiv>
-  )
-);
-
-// const usaTime = date => new Date(date).toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
-const sweTime = date => new Date(date).toLocaleString('sv-SE', { timeZone: 'Europe/Stockholm' });
-
-const mapAppointmentData = appointment => ({
-  id: appointment.id,
-  startDate: sweTime(appointment.start.dateTime),
-  endDate: sweTime(appointment.end.dateTime),
-  title: appointment.summary,
-});
-
-const initialState = {
-  data: [],
-  loading: false,
-  currentDate: '2017-05-23',
-  currentViewName: 'Week',
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'setLoading':
-      return { ...state, loading: action.payload };
-    case 'setData':
-      return { ...state, data: action.payload.map(mapAppointmentData) };
-    case 'setCurrentViewName':
-      return { ...state, currentViewName: action.payload };
-    case 'setCurrentDate':
-      return { ...state, currentDate: action.payload };
-    default:
-      return state;
+    async getData() {
+      const dataUrl = "http://localhost:5001/medlem/boka/hamta_alla";
+    
+      const schedule = await fetch(dataUrl, {headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+        }
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data state is set here: ", data);
+        this.setState({
+          data: data
+        });
+      });
+    };
+  
+    commitChanges({ added, changed, deleted }) {
+      this.setState((state) => {
+        let { data } = state;
+        if (added) {
+          const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+          data = [...data, { id: startingAddedId, ...added }];
+        }
+        if (changed) {
+          data = data.map(appointment => (
+            changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+        }
+        if (deleted !== undefined) {
+          data = data.filter(appointment => appointment.id !== deleted);
+        }
+        return { data };
+      });
+    }
+  
+    render() {
+      const { data } = this.state;
+  
+      return (
+        <Paper>
+          <Scheduler
+            data={data}
+            height={660}
+          >
+            <ViewState/>   
+            <EditingState
+              onCommitChanges={this.commitChanges}
+            />
+            <IntegratedEditing />
+            <ConfirmationDialog />
+            <WeekView
+              startDayHour={7.5}
+              endDayHour={17.5}
+            />
+            <Appointments />
+            <AppointmentTooltip
+              showOpenButton
+              showDeleteButton
+            />
+            <AppointmentForm />
+          </Scheduler>
+        </Paper>
+      );
+    }
   }
-};
-
-export default () => {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
-  const {
-    data, loading, currentViewName, currentDate,
-  } = state;
-  const setCurrentViewName = React.useCallback(nextViewName => dispatch({
-    type: 'setCurrentViewName', payload: nextViewName,
-  }), [dispatch]);
-  const setData = React.useCallback(nextData => dispatch({
-    type: 'setData', payload: nextData,
-  }), [dispatch]);
-  const setCurrentDate = React.useCallback(nextDate => dispatch({
-    type: 'setCurrentDate', payload: nextDate,
-  }), [dispatch]);
-  const setLoading = React.useCallback(nextLoading => dispatch({
-    type: 'setLoading', payload: nextLoading,
-  }), [dispatch]);
-
-  React.useEffect(() => {
-    getData(setData, setLoading);
-  }, [setData, currentViewName, currentDate]);
-
-  return (
-    <Paper>
-      <Scheduler
-        data={data}
-        height={660}
-      >
-        <ViewState
-          currentDate={currentDate}
-          currentViewName={currentViewName}
-          onCurrentViewNameChange={setCurrentViewName}
-          onCurrentDateChange={setCurrentDate}
-        />
-        <DayView
-          startDayHour={7.5}
-          endDayHour={17.5}
-        />
-        <WeekView
-          startDayHour={7.5}
-          endDayHour={17.5}
-        />
-        <Appointments />
-        <Toolbar
-          {...loading ? { rootComponent: ToolbarWithLoading } : null}
-        />
-        <DateNavigator />
-        <TodayButton />
-        <ViewSwitcher />
-        <AppointmentTooltip
-          showOpenButton
-          showCloseButton
-        />
-        <AppointmentForm readOnly />
-      </Scheduler>
-    </Paper>
-  );
-};
+  
