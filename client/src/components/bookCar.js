bf65975ffe7bcaed6d94228e796bf5aaa9b013e3
Sean <seazh870@student.liu.se>
2022-05-27 09:47:36 +0200

    added redirects and restrictions to memberfunctions

--- client/src/components/bookCar.js
+++ client/src/components/bookCar.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
 import Bookings from "./scheduler";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 export default function BookCar() {
+    const {isAuthenticated } = useAuth0();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(!isAuthenticated) {
+           return navigate('/medlem');
+        }
+    }, [])
+
     return (
         <div className="container">
             <h2>Bilbokning</h2>
--- client/src/components/logging.js
+++ client/src/components/logging.js
@@ -1,9 +1,20 @@
-import React from "react";
- 
+import React, { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function Logging() {
+    const {isAuthenticated } = useAuth0();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(!isAuthenticated) {
+           return navigate('/medlem');
+        }
+    }, [])
+
     return (
         <div className="container">
             <h2>Loggbok</h2>
